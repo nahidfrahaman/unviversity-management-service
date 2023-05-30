bf33392e74e1c564e19a82b4989bdda651d38916
NahidFRahaman <nahi.frahaman@gmail.com>
2023-05-30 21:57:39 +0600

    t

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-
-import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import express, { Application, Request } from 'express'
 const app: Application = express()
 
 // using cors
